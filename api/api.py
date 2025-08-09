477e1e12ce0fabf7ac5f9d322a0324a9072aba8d
Digital2512 <valerieannabella123@gmail.com>
2025-08-09 17:59:01 +1000

    Updated Predict_Chain and Predict_Next

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime, timezone
-from pawsense.behaviour_prediction.model import predict_chain
+from pawsense.behaviour_prediction.model import predict
 import os
 
 app = Flask(__name__)
@@ -11,22 +11,22 @@ CORS(app)
 def predict_chain():
     data = request.json
     current_activity = data.get('current_activity')
-    last_activity_time_str = data.get('last_activity_time')
+    current_activity_time = data.get('last_activity_time')
 
     if not current_activity:
         return jsonify({'error': 'No Current Activity Provided'})
     
-    if not last_activity_time_str:
+    if not current_activity_time:
         return jsonify({'error': 'No Last Activity Time Provided'})
     
     try:
-        last_activity_time = datetime.fromisoformat(last_activity_time_str)
+        last_activity_time = datetime.fromisoformat(current_activity_time)
         now = datetime.now(timezone.utc)
         minutes_since_last = (now - last_activity_time).total_seconds()/60
     except Exception:
         return jsonify({'error': 'Invalid last_activity_time format'}), 400
     
-    predictions = predict(current_activity, last_activity_time_str)
+    predictions = predict(current_activity, current_activity_time, max_depth=6)
 
     result = []
     for activity, time_to_next, prob in predictions[1:]:
@@ -42,22 +42,22 @@ def predict_chain():
 def predict_next():
     data = request.json
     current_activity = data.get('current_activity')
-    last_activity_time_str = data.get('last_activity_time')
+    current_activity_time = data.get('last_activity_time')
 
     if not current_activity:
         return jsonify({'error': 'No Current Activity Provided'})
     
-    if not last_activity_time_str:
+    if not current_activity_time:
         return jsonify({'error': 'No Last Activity Time Provided'})
     
     try:
-        last_activity_time = datetime.fromisoformat(last_activity_time_str)
+        last_activity_time = datetime.fromisoformat(current_activity_time)
         now = datetime.now(timezone.utc)
         minutes_since_last = (now - last_activity_time).total_seconds()/60
     except Exception:
         return jsonify({'error': 'Invalid last_activity_time format'}), 400
     
-    predictions = predict(current_activity)
+    predictions = predict(current_activity, current_activity_time)
 
     result = []
     for activity, time_to_next, prob in predictions[1:]:
