1c3de618292f050c19ccebf52533d4564bdb9762
Digital2512 <valerieannabella123@gmail.com>
2025-08-09 12:39:29 +1000

    Changed the requirement.txt

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from behaviour_prediction.model import predict_next_activity
+from pawsense.behaviour_prediction.model import predict_next_activity
 
 app = Flask(__name__)
 CORS(app)
